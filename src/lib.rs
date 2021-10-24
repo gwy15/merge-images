aca2083961de646e11d65ef94a62788ee18875ba
gwy15 <gwy15thu@gmail.com>
2021-10-24 16:17:55 +0800

    再加一层保险

--- src/lib.rs
+++ src/lib.rs
@@ -233,6 +233,26 @@ pub fn merge<T: AsRef<[u8]>>(image_bytes: &[T]) -> Result<Vec<u8>> {
         })?;
         info!("image size: {:?}", im.size()?);
 
+        let (width, height) = (im.cols(), im.rows());
+        let im = match width.max(height) {
+            size if size > 8000 => {
+                error!("表现不一致：大小还是超过 8000");
+                info!("继续缩放为 1/8");
+                let mut output = Mat::default();
+                imgproc::resize(
+                    &im,
+                    &mut output,
+                    cv_core::Size::new(0, 0),
+                    1. / 8.,
+                    1. / 8.,
+                    imgproc::INTER_LINEAR,
+                )?;
+                std::mem::drop(im);
+                output
+            }
+            _ => im,
+        };
+
         cv_images.push(im);
     }
 
